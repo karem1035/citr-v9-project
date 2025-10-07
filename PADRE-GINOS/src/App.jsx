d9f87a700f84278eda86c504705404d02db261f6
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-10-07 17:03:29 +0300

    Updates

--- PADRE-GINOS/src/App.jsx
+++ PADRE-GINOS/src/App.jsx
@@ -5,14 +5,20 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's</h1>
-      <Pizza name="Margherita" description="Tomato sauce, mozzarella, basil" />
+      <Pizza
+        name="Margherita"
+        description="Tomato sauce, mozzarella, basil"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
       <Pizza
         name="Diavola"
         description="Tomato sauce, mozzarella, red pepper"
+        image={"/public/pizzas/hawaiian.webp"}
       />
       <Pizza
         name="Capricciosa"
         description="Tomato sauce, mozzarella, ham, mushrooms"
+        image={"/public/pizzas/big_meat.webp"}
       />
     </div>
   );

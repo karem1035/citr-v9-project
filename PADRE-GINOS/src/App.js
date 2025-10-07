746743ed9f23c141c7b244f3d37b8c6c54bca5fc
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-10-07 07:37:49 +0300

    feat: initialize project with HTML structure, Prettier config, and basic React components

---
+++ PADRE-GINOS/src/App.js
@@ -0,0 +1,32 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "Pizza Margherita",
+      description:
+        "Tomato, mozzarella, fresh basil, salt, and extra-virgin olive oil.",
+    }),
+    React.createElement(Pizza, {
+      name: "Pizza Marinara",
+      description: "Tomato, garlic, oregano, and extra-virgin olive oil.",
+    }),
+    React.createElement(Pizza, {
+      name: "Pizza Quattro Stagioni",
+      description:
+        "Tomato, mozzarella, mushrooms, artichokes, ham, olives, and oregano.",
+    })
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));

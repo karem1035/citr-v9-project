d299c6b82416bfa35be205e8d717c8f28b525a1a
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-10-08 17:09:25 +0300

    feat: add cart context and header component
    - Added CartContext to manage cart state across components.
    - Introduced Header component to display the cart item count.
    - Updated App component to include Header and provide CartContext.
    - Modified Order component to use CartContext for cart state.
    - Added new dependencies for TanStack Router.

--- PADRE-GINOS/src/App.jsx
+++ PADRE-GINOS/src/App.jsx
@@ -1,16 +1,21 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import PizzaOfTheDay from "./PizzaOfTheDay.jsx";
+import Header from "./Header.jsx";
 import Order from "./Order.jsx";
+import { CartContext } from "./contexts.jsx";
 
 const App = () => {
+  const cartHook = useState([]);
   return (
     <StrictMode>
-      <div>
-        <h1 className="logo">Padre Gino's Order now</h1>
-        <Order />
-        <PizzaOfTheDay />
-      </div>
+      <CartContext.Provider value={cartHook}>
+        <div>
+          <Header />
+          <Order />
+          <PizzaOfTheDay />
+        </div>
+      </CartContext.Provider>
     </StrictMode>
   );
 };

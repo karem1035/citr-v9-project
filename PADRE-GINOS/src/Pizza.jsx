85c37622de17f6ba1adbbcd36bc3c47e3ecc2c81
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-10-08 21:22:25 +0300

    fix: remove unused counter and simplify Pizza component rendering

--- PADRE-GINOS/src/Pizza.jsx
+++ PADRE-GINOS/src/Pizza.jsx
@@ -1,12 +1,7 @@
-let counter = 0;
-
 const Pizza = (props) => {
-  counter = counter + 1;
   return (
     <div className="pizza">
-      <h1>
-        {props.name} {counter}
-      </h1>
+      <h1>{props.name}</h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />
     </div>

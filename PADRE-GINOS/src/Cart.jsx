aa3077b95f134646d8b16fa4825a6c147fe030a4
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-10-08 12:16:09 +0300

    fix: update Cart component header and improve item display format

--- PADRE-GINOS/src/Cart.jsx
+++ PADRE-GINOS/src/Cart.jsx
@@ -11,12 +11,12 @@ export default function Cart({ cart, checkout }) {
   }
   return (
     <div className="cart">
-      <h2>{cart}</h2>
+      <h2>Cart</h2>
       <ul>
         {cart.map((item, index) => (
           <li key={index}>
-            <span className="size">{item.size}</span>
-            <span className="type">{item.pizza.name}</span>
+            <span className="size">{item.size}</span> -
+            <span className="type">{item.pizza.name}</span> -
             <span className="price">{item.price}</span>
           </li>
         ))}
